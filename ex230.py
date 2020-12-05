a8228b6cd19bfbe764db37754dabfac18a99a358
juno lee <ljlj5746@naver.com>
2020-12-06 03:27:27 +0900

    ex230

---
+++ ex230.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def my_print (a, b) :
+    print("왼쪽:", a)    #a랑 왼쪽이랑 매치시켰다
+    print("오른쪽:", b)  #b랑 오른쪽이랑 매치시켰다
+
+my_print(b=100, a=200)   #a = 200, b = 100 대입시켰다.
+#왼쪽 : 200
+#오른쪽 : 100
+
