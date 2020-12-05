bd85b8121959bd0009a517f7bc8281a34b168166
juno lee <ljlj5746@naver.com>
2020-12-05 20:02:26 +0900

    ex040

---
+++ ex040.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+data  = "   삼성전자   "
+data1 = data.strip() #strip을 사용함으로써 삼성전자 양쪽위 빈 공백들을 제거하였다.
+print(data1)         #공백이 제거되고 남은 삼성전자 만 출력된다.
+#삼성전자
+
