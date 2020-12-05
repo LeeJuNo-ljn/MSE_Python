90726b83203773db5e3793c4eef73fd1070f7c06
juno lee <ljlj5746@naver.com>
2020-12-05 20:04:10 +0900

    ex060

---
+++ ex060.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+nums = [1, 2, 3, 4, 5]
+average = sum(nums) / len(nums) #sum(nums)는 nums에 속한 숫자의 총합을 구한다.
+                                #len(nums)는 nums에 원소가 몇개인지를 구한다.
+print(average)
+#3.0
+
