439ddfc614c9717c1fda8dbe38679b8230d444bb
juno lee <ljlj5746@naver.com>
2020-12-05 22:38:15 +0900

    ex170

---
+++ ex170.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+result = 1
+for i in range(1, 11):  #1이상 11이하일 때  
+    result = result * i #result가 1부터 시작해서 1X1, 1X2, 2X3이렇게 나아가 362880*10의 값이 출력된다.
+print(result)
+#3628800
+
