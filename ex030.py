73f6d3ec423862322c992450ba4f433594c2d570
juno lee <ljlj5746@naver.com>
2020-12-05 20:01:43 +0900

    ex030

---
+++ ex030.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+string = 'abcd'
+string.replace('b', '8')  #string = 'abcd'에서 문자b를 숫자8로 바꾸려 함
+print(string)             #하지만 string은 문자열로 바꿀 수 없는 자료형임으로 abcd 그대로 결과값이 출력된다.
+#abcd
+
